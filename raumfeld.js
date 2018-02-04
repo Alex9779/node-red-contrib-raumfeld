cc980a01b0ed294dd5f05bcc0e32ba06b9d70a6b
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2018-02-04 21:22:43 +0100

    added node to get if a room is playing or not

--- raumfeld.js
+++ raumfeld.js
@@ -245,4 +245,38 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("raumfeld room title changed", RaumfeldRoomTitleChanged);
+
+    function RaumfeldRoomIsPlaying(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.raumkernelNode = RED.nodes.getNode(config.raumkernel);
+
+        var roomName = config.roomName;
+
+        function handleEvent(_mediaRenderer, _key, _oldValue, _newValue, _roomUdn) {
+            var msg = {};
+            msg.roomName = roomName;
+
+            if (!(_mediaRenderer instanceof RaumkernelLib.MediaRendererRaumfeldVirtual)) {
+                if (_mediaRenderer.roomName() == roomName && _key == "TransportState") {
+                    if (_newValue == "PLAYING") {
+                        msg.payload = true;
+                    }
+                    else if (_newValue == "NO_MEDIA_PRESENT") {
+                        msg.payload = false;
+                    }
+
+                    if (msg.hasOwnProperty("payload")) node.send(msg);
+                }
+            }
+        }
+
+        node.raumkernelNode.raumkernel.on("rendererStateKeyValueChanged", handleEvent);
+
+        node.on('close', function() {
+            node.raumkernelNode.removeListener("rendererStateKeyValueChanged", handleEvent);
+        });
+    }
+    RED.nodes.registerType("raumfeld room is playing", RaumfeldRoomIsPlaying);
 }
