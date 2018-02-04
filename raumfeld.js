eb56761fbc73087391da7b31bdc003daf27ab4b0
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2018-02-04 11:07:33 +0100

    new node to monitor title changes in a room

--- raumfeld.js
+++ raumfeld.js
@@ -219,4 +219,34 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("raumfeld room load playlist", RaumfeldRoomLoadPlaylist);
+
+    function RaumfeldRoomTitleChanged(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.raumkernelNode = RED.nodes.getNode(config.raumkernel);
+
+        var roomName = config.roomName;
+
+        function handleEvent(_mediaRenderer, _mediaItemData) {
+            var msg = {};
+
+            msg.roomName = roomName;
+
+            if (_mediaRenderer.roomName().search(roomName) >= 0) {
+                msg.payload = _mediaItemData.title;
+                msg.title = _mediaItemData.title;
+                msg.parentId = _mediaItemData.parentId;
+
+                node.send(msg);
+            }
+        }
+
+        node.raumkernelNode.raumkernel.on("rendererMediaItemDataChanged", handleEvent);
+
+        node.on('close', function() {
+            node.raumkernelNode.removeListener("rendererMediaItemDataChanged", handleEvent);
+        });
+    }
+    RED.nodes.registerType("raumfeld room title changed", RaumfeldRoomTitleChanged);
 }
