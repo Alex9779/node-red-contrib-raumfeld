eb6ea55bda26b3e210b5b39302fb4c7592ccf3cb
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2022-01-07 23:32:47 +0100

    add node to get mute state

---
+++ nodes/raumfeld-room-get-mute.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = function(RED) {
+    function RaumfeldRoomGetMuteNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.raumkernelNode = RED.nodes.getNode(config.raumkernel);
+
+        node.on("input", function(msg) {
+            var roomName = config.roomName || msg.roomName || msg.payload;
+            
+            var mediaRenderer = node.raumkernelNode.deviceManager.getMediaRenderer(roomName);
+
+            msg.roomName = roomName;
+
+            if (mediaRenderer) {
+                msg.payload = mediaRenderer.rendererState.Mute == '0' ? false : true;
+            }
+
+            if (msg.hasOwnProperty("payload")) node.send(msg);
+        });
+    }
+    RED.nodes.registerType("raumfeld-room-get-mute", RaumfeldRoomGetMuteNode);
+}
